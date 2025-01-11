8ebe4c35355c49bea071d99db434c6b1c65b18d0
Sadat Hossain <sadatho2014@gmail.com>
2025-01-11 19:55:55 +0600

    test commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = '/sadathossain.github.io';
+const base = '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
