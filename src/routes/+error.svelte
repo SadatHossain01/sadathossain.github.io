7b19d706605355e5b4f5863b366e70cc339dcffb
Sadat Hossain <sadatho2014@gmail.com>
2025-01-14 15:54:03 +0600

    added an error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center text-center">
+	<h1 class="m-0 text-9xl font-bold">404</h1>
+	<h2 class="mb-2 mt-4 text-3xl font-semibold">Page Not Found</h2>
+	<p class="text-muted-foreground">Looks like you've ventured into the unknown digital realm.</p>
+	<Button href="/" class="mt-8">Go Home</Button>
+</div>
